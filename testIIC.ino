4639548237f628478d8834a2233c019f9b41d719
Keshuai Xu <i@xks.im>
2015-04-27 11:33:03 -0400

    PI Turning code

--- testIIC.ino
+++ testIIC.ino
@@ -10,16 +10,18 @@ MotorControllerMaster c;
 long lasthb = 0;
 long lastpoll = 0;
 long lastsc = 0;
-int i = -400;
-LiquidCrystal_I2C lcd(0x27,16,2);
+long lastturn = 0;
+//LiquidCrystal_I2C lcd(0x27,16,2);
 //LiquidCrystal lcd(40,41,42,43,44,45);
 void setup(){
 	//  lcd.begin(16,2);
+	pinMode(9,OUTPUT);
+	digitalWrite(9,LOW);
 	Wire.begin();
 	Serial.begin(115200);
 	delay(1000);
   //lcd.init();
-  
+  /*
   //lcd.setCursor(0,0); 	
   if (!gyro.init())
   {
@@ -28,6 +30,7 @@ void setup(){
   }
 
   gyro.enableDefault();	
+  */
 	c.begin();
 	c.setAcceleration(1500,1000,2500,1000);
 	c.coast();
@@ -35,44 +38,69 @@ void setup(){
 }
 
 
+int i;
+int j;
 
+
+//PD for rotating 90 degs
+const float kp = 1.3;
+const float kd = 2.2;
+/*
+
+const float kp = 3.8;
+const float kd = 1.5;
+*/
+int lasterr;
 void loop(){
 	
 	long l;
 	long r;
+	long now = millis();
 
-
-	if (millis() - lastpoll > 100){
+	if (now - lastpoll > 10){
 		c.getEncoder(&l,&r);
-		int8_t s = c.isStandby();
-		Serial.println(l);
-		Serial.println(r);
-		Serial.println(s);
-		Serial.println("");
-		gyro.read();
-
-		Serial.print("G ");
-		Serial.print("X: ");
-		Serial.print((int)gyro.g.x);
-		Serial.print(" Y: ");
-		Serial.print((int)gyro.g.y);
-		Serial.print(" Z: ");
-		Serial.println((int)gyro.g.z);
-		lastpoll = millis();
+		int err = i - r;
+		int out = constrain((err)*kp + (err - lasterr)*kd,-90,90);
+		c.goVelocity(0,out);
+		Serial.println(err);
+		lastpoll = now;
+		lasterr = err;
 	}
 
-	if (millis() - lastsc > 4000){
-		i += 200;
-		//c.goVelocity(i,0);
-		lastsc = millis();
+/*
+	if (now - lastpoll > 10){
+		c.getEncoder(&l,&r);
+		int err = i - l;
+		int out = constrain((err)*kp + (err - lasterr)*kd,-100,100);
+		c.goVelocity(out,0);
+		Serial.println(err);
+		lastpoll = now;
+		lasterr = err;
 	}
+*/
 	//delay(1000);
 	//
 
 
-	if (millis() - lasthb > 2000){
+	if (now - lasthb > 4000){
 		c.heartbeat();
-		lasthb = millis();	
+		lasthb = now;	
+		if (j++%2 == 1) digitalWrite(9,LOW);
+		else analogWrite(9,0);
 	}
 
+	if (now - lastturn > 4000 && abs(lasterr) < 4){
+		/*
+		if (j++%2 == 1) i = 90;
+		else i = 0;
+		*/
+		i += 90;
+		lastturn = now;
+		/*
+		if (i > 1000){
+			i = 1000;
+		}
+		*/
+
+	}
 }
